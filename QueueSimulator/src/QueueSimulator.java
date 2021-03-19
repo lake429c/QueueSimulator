626e75ee0e30a939cb05f5eb07806144184ae46e
lake429c <have2nthusiasm@gmail.com>
2021-03-19 14:22:53 +0900

    add Animation

--- QueueSimulator/src/QueueSimulator.java
+++ QueueSimulator/src/QueueSimulator.java
@@ -51,6 +51,9 @@ class SimWindow extends JFrame implements ActionListener{
 	private DrawCanvas canv_imageCanvas;
 	private CalculateQueue cal_queue;
 
+
+	private Timer timer = new Timer();
+
 	SimWindow(String title, int width, int height){
 		super(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -65,7 +68,7 @@ class SimWindow extends JFrame implements ActionListener{
 		label_line_num = new JLabel("行列数：");
 		field_line_num = new JTextField("1", 5);
 		label_staff_num = new JLabel("窓口数：");
-		field_staff_num = new JTextField("2", 5);
+		field_staff_num = new JTextField("1", 5);
 
 		button_execute = new JButton("Start");
 		button_execute.addActionListener(this);
@@ -109,14 +112,13 @@ class SimWindow extends JFrame implements ActionListener{
 		panel_images.add(canv_imageCanvas);
 
 		// タイマー
-		Timer timer = new Timer();
 		timer.schedule(canv_imageCanvas.getTask(), 1l, 10l);
 
 		cal_queue = new CalculateQueue();
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		CalculateQueue.setCustomers(new ArrayList<String>());
+		CalculateQueue.setCustomers(new ArrayList<CalculateQueue.Customer>());
 		int arrival_interval = Integer.parseInt(field_arrival_interval.getText());
 		int service_interval = Integer.parseInt(field_service_interval.getText());
 		int line_num = Integer.parseInt(field_line_num.getText());
@@ -124,7 +126,7 @@ class SimWindow extends JFrame implements ActionListener{
 		int staff_num = Integer.parseInt(field_staff_num.getText());
 		canv_imageCanvas.setStaffNum(staff_num);
 		cal_queue.setStaffNum(staff_num);
-		String[] processing_customer = new String[staff_num];
+		CalculateQueue.Customer[] processing_customer = new CalculateQueue.Customer[staff_num];
 		for(int i=0;i<processing_customer.length;i++) processing_customer[i]=null;
 		CalculateQueue.setProcessingCustomers(processing_customer);
 		double arrival_rate = 1.0/arrival_interval;
@@ -132,11 +134,10 @@ class SimWindow extends JFrame implements ActionListener{
 		double service_rate = 1.0/service_interval;
 		cal_queue.setServiceRate(service_rate);
 		double rho = arrival_rate/service_rate;
-		label_ideal_value.setText("待ち時間 理論値："+service_interval*Math.pow(rho,staff_num)/(1-Math.pow(rho,staff_num))+"分");
+		label_ideal_value.setText(String.format("待ち時間 理論値：%.1f分", service_interval*Math.pow(rho,staff_num)/(1-Math.pow(rho,staff_num))));
 		//label_actual_value.setText("実測値："+service_interval*CalculateQueue.getCustomers().size()+"s");
 
-		Timer timer2 = new Timer();
-		timer2.schedule(cal_queue.getTask(), 1l, 100l);
+		timer.schedule(cal_queue.getTask(), 1l, 500l);
 	}
 }
 
@@ -171,35 +172,38 @@ class DrawCanvas extends JPanel{
 
 		g.drawImage(this.background, 0, 0, this);
 		//列の表示
-		for(int i=0;i<CalculateQueue.getCustomers().size();i++) {
-			g.drawImage(this.img_customer, this.canvas_width/2-this.img_customer.getWidth(null)*i, this.canvas_height/2,
-					(int)(this.img_customer.getWidth(null)*this.expanse_rate),
-					(int)(this.img_customer.getHeight(null)*this.expanse_rate),
+		for(CalculateQueue.Customer c: CalculateQueue.getCustomers()) {
+			c.move();
+			g.drawImage(c.image, this.canvas_width/2-c.x, this.canvas_height/2,
+					(int)(c.image.getWidth(null)*this.expanse_rate),
+					(int)(c.image.getHeight(null)*this.expanse_rate),
 					this);
-			g.drawString(CalculateQueue.getCustomers().get(i),
-					this.canvas_width/2-this.img_customer.getWidth(null)*i, this.canvas_height/2+this.img_customer.getHeight(null));
+			g.drawString(c.name,
+					this.canvas_width/2-c.x, this.canvas_height/2+c.image.getHeight(null));
 		}
 		//窓口描画
 		for(int i=0;i<this.staff_num;i++) {
-			g.drawImage(this.img_staff, this.canvas_width*17/20, this.canvas_height*i/staff_num,
+			g.drawImage(this.img_staff, this.canvas_width/2+this.img_staff.getWidth(null)*2, (int)(this.canvas_height/2+(i-1)*1.2*this.img_staff.getHeight(null)),
 					(int)(this.img_staff.getWidth(null)*this.expanse_rate),
 					(int)(this.img_staff.getHeight(null)*this.expanse_rate),
 					this);
 			g.drawString(CalculateQueue.getStaffs()[i],
-					this.canvas_width*17/20, this.canvas_height*i/staff_num+this.img_staff.getHeight(null));
+					this.canvas_width/2+this.img_staff.getWidth(null)*2, (int)(this.canvas_height/2+(i-1)*1.2*this.img_staff.getHeight(null))+this.img_staff.getHeight(null));
 		}
 		//受付中の客描画
-		for(int i=0;i<this.staff_num;i++) {
-			if(CalculateQueue.getProcessingCustomers()[i]!=null) {
-				g.drawImage(this.img_customer, this.canvas_width*7/10, this.canvas_height*i/staff_num,
-						(int)(this.img_customer.getWidth(null)*this.expanse_rate),
-						(int)(this.img_customer.getHeight(null)*this.expanse_rate),
-						this);
-				g.drawString(CalculateQueue.getProcessingCustomers()[i],
-						this.canvas_width*7/10, this.canvas_height*i/staff_num+this.img_customer.getHeight(null));
+		if(CalculateQueue.getProcessingCustomers() != null) {
+			for(CalculateQueue.Customer c: CalculateQueue.getProcessingCustomers()) {
+				if(c!=null) {
+					c.goRegister();
+					g.drawImage(c.image, this.canvas_width/2-c.x, c.y+this.canvas_height/2,
+							(int)(c.image.getWidth(null)*this.expanse_rate),
+							(int)(c.image.getHeight(null)*this.expanse_rate),
+							this);
+					g.drawString(c.name,
+							this.canvas_width/2-c.x, c.y+this.canvas_height/2+c.image.getHeight(null));
+				}
 			}
 		}
-
 	}
 
 	public TimerTask getTask() {
@@ -219,8 +223,8 @@ class CalculateQueue {
 	private Random random_jen = new Random();
 	private int random_int = 0;
 	private TimerTask task;
-	private static List<String> customers = new ArrayList<String>();
-	private static String[] processing_customers;
+	private static List<Customer> customers = new ArrayList<Customer>();
+	private static Customer[] processing_customers = null;
 	private static String[] staffs;
 	private double arrival_rate;
 	private double service_rate;
@@ -229,6 +233,8 @@ class CalculateQueue {
 	private List<String> customer_name_list = sql_manager.getCustomerNameList();
 	private List<String> staff_name_list = sql_manager.getStaffNameList();
 	private List<Double> actual_values = new ArrayList<>();
+	private Image img_customer = Toolkit.getDefaultToolkit().getImage("./src/image/customer.png");
+	private Image img_staff = Toolkit.getDefaultToolkit().getImage("./src/image/staff.png");
 
 	public CalculateQueue() {
 		this.task = new TimerTask() {
@@ -237,14 +243,16 @@ class CalculateQueue {
 				random_int = random_jen.nextInt(100);
 				if(random_int < 100*arrival_rate) {
 					System.out.println("add");
-					for(int i=0;i<staff_num;i++) {
-						String name = customer_name_list.get(random_jen.nextInt(customer_name_list.size()));
-						if(processing_customers[i]==null) {
-							processing_customers[i] = name;
-							break;
-						} else if (i==staff_num-1){
-							customers.add(name);
-						}
+					String name = customer_name_list.get(random_jen.nextInt(customer_name_list.size()));
+					customers.add(new Customer(name, img_customer.getWidth(null)*customers.size(), 0, img_customer, customers.size(), 0));
+				}
+				//レジが空いていればレジへ通す
+				for(int i=0;i<staff_num;i++) {
+					if(processing_customers[i]==null && customers.size()!=0) {
+						processing_customers[i] = customers.get(0).copy();
+						customers.remove(0);
+						processing_customers[i].setProcessNum(i);
+						for(int j=0;j<customers.size();j++) customers.get(j).setOrderNum();
 					}
 				}
 				//客を捌く
@@ -253,17 +261,19 @@ class CalculateQueue {
 					if(random_int < 100*service_rate && processing_customers[i]!=null) {
 						System.out.println("remove");
 						if(customers.size()!=0) {
-							processing_customers[i] = customers.get(0);
+							processing_customers[i] = customers.get(0).copy();
 							customers.remove(0);
+							processing_customers[i].setProcessNum(i);
+							for(int j=0;j<customers.size();j++) customers.get(j).setOrderNum();
 						} else {
 							processing_customers[i] = null;
 						}
 					}
 				}
 				int empty_register = 1;
-				for(String name: processing_customers) if(name == null) empty_register = 0;
+				for(int i=0;i<processing_customers.length;i++) if(processing_customers[i] == null) empty_register = 0;
 				actual_values.add((CalculateQueue.getCustomers().size()+empty_register)/service_rate);
-				SimWindow.label_actual_value.setText("待ち時間 実測値："+calMean(actual_values)+"分");
+				SimWindow.label_actual_value.setText(String.format("待ち時間 実測値：%.1f分", calMean(actual_values)));
 			}
 		};
 	}
@@ -284,11 +294,11 @@ class CalculateQueue {
 		this.service_rate = service_rate;
 	}
 
-	public static List<String> getCustomers(){
+	public static List<Customer> getCustomers(){
 		return customers;
 	}
 
-	public static void setCustomers(List<String> s) {
+	public static void setCustomers(List<Customer> s) {
 		customers = s;
 	}
 
@@ -304,11 +314,11 @@ class CalculateQueue {
 		return staffs;
 	}
 
-	public static String[] getProcessingCustomers(){
+	public static Customer[] getProcessingCustomers(){
 		return processing_customers;
 	}
 
-	public static void setProcessingCustomers(String[] s) {
+	public static void setProcessingCustomers(Customer[] s) {
 		processing_customers = s;
 	}
 
@@ -318,6 +328,53 @@ class CalculateQueue {
 			result += i/actual.size();
 		}
 		return result;
+	}
+
+	public class Customer{
+		private int order_num;
+		private int process_num;
+		public int x = 0;
+		public int y = 0;
+		public String name = null;
+		public Image image;
+
+
+		public Customer(String name, int x, int y, Image image, int order, int process) {
+			this.name = name;
+			this.x = x;
+			this.y = y;
+			this.image = image;
+			this.order_num = order;
+			this.process_num = process;
+		}
+
+		public void move() {
+			if(this.x != this.image.getWidth(null)*this.order_num) {
+				this.x -= this.image.getWidth(null)/30;
+			}
+		}
+
+		public void setOrderNum() {
+			this.order_num--;
+		}
+
+		public void setProcessNum(int process) {
+			this.process_num = process;
+		}
+
+		public void goRegister() {
+			if(this.x != -this.image.getWidth(null)) {
+				this.x += -this.image.getWidth(null)/30;
+			}
+			if(this.y != (this.process_num-1)*this.image.getHeight(null)) {
+				this.y += (this.process_num-1)*this.image.getHeight(null)/30;
+			}
+		}
+
+		public Customer copy() {
+			Customer result = new Customer(name, x, y, image, order_num, process_num);
+			return result;
+		}
 	}
 
 }
